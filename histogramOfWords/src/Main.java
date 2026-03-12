29250185d6254e360a131d2325a3e50613a83c3f
mariusbn16 <mariusbougouin@gmail.com>
2026-03-12 11:47:26 +0100

    Add histogramOfWords assignment

---
+++ histogramOfWords/src/Main.java
@@ -0,0 +1,53 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class wordinfo {
+    String word;
+    int count;
+    public wordinfo(String word, int count){
+        this.word = word;
+        this.count = count;
+    }
+    public String toString(){
+        return word + " : " + count + "\n";
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length != 1){
+            System.err.println("Usage: java Main <file>");
+            System.exit(1);
+        }
+        Scanner input = null;
+        try {
+            input = new Scanner(new FileInputStream(new File(args[0])));
+            ArrayList<wordinfo> words = new ArrayList<wordinfo>();
+            long startTime = System.currentTimeMillis();
+            while (input.hasNext()){
+                String word = input.next().toLowerCase();
+                boolean found = false;
+                for (int i = 0; i < words.size(); i++){
+                    wordinfo w = words.get(i);
+                    if (w.word.equals(word)){
+                        w.count++;
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found){
+                    words.add(new wordinfo(word, 1));
+                }
+            }
+            long endTime = System.currentTimeMillis();
+            System.out.println(words);
+            System.out.println("Time taken: " + (endTime-startTime)/1000.0 + " seconds");
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + args[0]);
+        }
+
+    }
+}
---
+++ histogramOfWords/src/MainHashVersion.java
@@ -0,0 +1,56 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
+class wordinfoHash {
+    String word;
+    int count;
+    public wordinfoHash(String word, int count){
+        this.word = word;
+        this.count = count;
+    }
+    public String toString(){
+        return word + " : " + count + "\n";
+    }
+    int hashCode(String word){
+        return word.hashCode();
+    }
+    boolean equals(String word){
+        return word.equals(this.word);
+    }
+}
+
+public class MainHashVersion {
+    public static void main(String[] args) {
+        if (args.length != 1){
+            System.err.println("Usage: java Main <file>");
+            System.exit(1);
+        }
+        Scanner input = null;
+        try {
+            input = new Scanner(new FileInputStream(new File(args[0])));
+            HashMap<String, Integer> words = new HashMap<>();
+            long startTime = System.currentTimeMillis();
+            while (input.hasNext()){
+                String word = input.next().toLowerCase();
+                Integer occurences = words.get(word);
+
+                if(occurences == null){
+                    words.put(word, 1);
+                } else {
+                    words.put(word, 0);
+                }
+            }
+            long endTime = System.currentTimeMillis();
+            System.out.println(words);
+            System.out.println("Time taken: " + (endTime-startTime)/1000.0 + " seconds");
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + args[0]);
+        }
+
+    }
+}
